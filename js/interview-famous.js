7cb0f4e9a27c2ccf85e24d376e49d9c422c704de
ReiBook Pro <reynakazato@ReiBook-Pro.local>
2025-07-06 23:11:43 +0900

    0706アップロード

--- js/interview-famous.js
+++ js/interview-famous.js
@@ -3,8 +3,16 @@ document.addEventListener('DOMContentLoaded', function () {
 const swiper = new Swiper('.interview-swiper', {
   direction: 'vertical',
   loop: true,
-slidesPerView: 3,
+slidesPerView: 2.5,
 spaceBetween: 10,
+breakpoints: {
+      768: {
+        slidesPerView: 2.5,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
   speed: 300,
   navigation: {
       nextEl: '.interview-content .swiper-button-next',
